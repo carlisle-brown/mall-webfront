93c689b47fb6ba5c56396a0942e21631f1a05691
brown.carlisle <brown.carlisle@outlook.com>
2020-08-06 21:51:08 +0800

    完善配置，添加webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,31 @@
 const webpack = require("webpack")
 const path = require("path")
-const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// 环境变量配置 dev/online
+const WEBPACK_ENV = process.env.WEBPACK_ENV || 'dev';
+// 获取html-webpack-plugin参数的方法
+var getHtmlConfig = function(name) {
+    return {
+        template: './src/view/'+name+'.html',
+        filename: 'view/'+name+'.html',
+        inject: true,
+        hash: true,
+        chunks: ['common',name]
+    };
+}
+// webpack config
 var config = {
     entry: {
         'common': ['./src/page/common/index.js'],
         'index': ['./src/page/index/index.js'],
         'login': ['./src/page/login/index.js']
     },
+    mode: 'development',
     output: {
         path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist',
         filename:'js/[name].js'
     },
     optimization: {
@@ -18,7 +35,7 @@ var config = {
                     name: "vendor",
                     test: /[\\/]node_modules[\\/]/,
                     chunks: "all",
-                    priority: 10 // 优先级
+                    priority: 10
                 },
                 common: {
                     name: "common",
@@ -37,17 +54,28 @@ var config = {
         rules: [
             {
               test: /\.css$/,
-              use: ExtractTextPlugin.extract({
-                fallback:"style-loader",
-                use:"css-loader"
-              })
+              use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+              test: /\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/,
+              loader: 'url-loader?limit=100&name=resource/[name].[ext]'
             },
         ]
     },
     plugins: [
-      new ExtractTextPlugin('css/[name].css')
+      // 将CSS单独打包至一个文件
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].css',
+      }),
+      // html模板处理插件
+      new HtmlWebpackPlugin(getHtmlConfig('index')),
+      new HtmlWebpackPlugin(getHtmlConfig('login')),
     ]
 
 };
+
+if('dev' === WEBPACK_ENV){
+  config.entry.common.push('webpack-dev-server/client?http://localhost:8088/')
+}
 
 module.exports = config;

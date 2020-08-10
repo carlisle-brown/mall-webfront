8184cf953ce3b4fb0eee3952800dac32f0f29793
brown.carlisle <brown.carlisle@outlook.com>
2020-08-10 15:44:05 +0800

    用户登陆实现

---
+++ src/page/user-login/index.js
@@ -0,0 +1,68 @@
+require('./index.css')
+require('page/common/nav-simple/index.js')
+var _bm = require('util/bm.js')
+var _user   = require('service/user-service.js')
+
+var formError = {
+	show:function(errMsg){
+		$('.error-item').show().find('.err-msg').text(errMsg);
+	},
+	hide:function(errMsg){
+		$('.error-item').hide().find('.err-msg').text('');
+	}
+}
+
+// page 逻辑部分
+var page = {
+	init:function(){
+		this.bindEvent();
+	},
+	bindEvent:function(){
+		var _this = this;
+		$('#submit').click(function(){
+			_this.submit();
+		})
+		$('.user-content').keyup(function(e){
+			if(e.keyCode===13){
+				_this.submit();
+			}
+		})
+	},
+	// 提交表单
+	submit:function(){
+		var formData = {
+			username:$.trim($('#username').val()),
+			password:$.trim($('#password').val()),
+		},
+		validateResult = this.formValidate(formData);
+		if(validateResult.status){
+			_user.login(formData, function(res){
+				window.location.href = _bm.getUrlParam('redirect')||'./index.html';
+			}, function(errMsg){
+				formError.show(errMsg);
+			});
+		}else{
+			formError.show(validateResult.msg);
+		}
+	},
+	formValidate:function(formData){
+		var result = {
+			status:false,
+			msg:''
+		};
+		if(!_bm.validate(formData.username,'require')){
+			result.msg='用户名不能为空';
+			return result;
+		}
+		if(!_bm.validate(formData.password,'require')){
+			result.msg='密码不能为空';
+			return result;
+		}
+		result.status = true;
+		result.msg='验证通过';
+		return result
+	}
+};
+$(function(){
+	page.init();
+});
--- src/util/bm.js
+++ src/util/bm.js
@@ -71,7 +71,7 @@ var _bm = {
   },
   // 统一登录处理
   doLogin : function(){
-    window.location.href='./login.html?redirect=' + encodeURIComponent(window.location.href);
+    window.location.href='./user-login.html?redirect=' + encodeURIComponent(window.location.href);
   },
   goHone: function(){
     window.location.href = './index.html'
